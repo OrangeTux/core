04c1fe2c62ec32ba03579e135d0679ce07fb1ccd
Auke Willem Oosterhoff <aukewillem.oosterhoff@mobilityhouse.com>
2020-08-23 16:28:28 +0200

    Allow to configure monitored variables

--- homeassistant/components/ev/sensor.py
+++ homeassistant/components/ev/sensor.py
@@ -13,7 +13,7 @@ import voluptuous as vol
 import websockets
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_PORT
+from homeassistant.const import CONF_MONITORED_VARIABLES, CONF_PORT
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
@@ -22,10 +22,40 @@ logging.getLogger("ocpp").setLevel(logging.DEBUG)
 
 ICON = "mdi:ev-station"
 
-SCAN_INTERVAL = timedelta(seconds=10)
+SCAN_INTERVAL = timedelta(seconds=1)
+
+MEASURANDS = [
+    "Current.Export",
+    "Current.Import",
+    "Current.Offered",
+    "Energy.Active.Export.Register",
+    "Energy.Active.Import.Register",
+    "Energy.Reactive.Export.Register",
+    "Energy.Reactive.Import.Register",
+    "Energy.Active.Export.Interval",
+    "Energy.Active.Import.Interval",
+    "Energy.Reactive.Export.Interval",
+    "Energy.Reactive.Import.Interval",
+    "Frequency",
+    "Power.Active.Export",
+    "Power.Active.Import",
+    "Power.Factor",
+    "Power.Offered",
+    "Power.Reactive.Export",
+    "Power.Reactive.Import",
+    "RPM",
+    "SoC",
+    "Temperature",
+    "Voltage",
+]
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {vol.Optional(CONF_PORT, default=9000): cv.positive_int}
+    {
+        vol.Optional(CONF_PORT, default=9000): cv.positive_int,
+        vol.Optional(CONF_MONITORED_VARIABLES): vol.All(
+            cv.ensure_list, [vol.In(MEASURANDS)]
+        ),
+    },
 )
 
 
@@ -37,7 +67,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     data = await CentralSystem.create(port=port)
 
     dev = []
-    dev.append(ChargeMetric(data))
+    for measurand in config[CONF_MONITORED_VARIABLES]:
+        dev.append(ChargeMetric(measurand, data))
+
     async_add_entities(dev, True)
 
 
@@ -63,22 +95,31 @@ class CentralSystem:
     async def on_connect(self, websocket, path: str):
         """Request handler executed for every new OCPP connection."""
         # For now only 1 charger can connect.
+        cp_id = path.strip("/")
         if self._connected_charger is not None:
             return
 
         try:
-            cp = ChargePoint(path, websocket)
+            cp = ChargePoint(cp_id, websocket)
             self._connected_charger = cp
-            _LOGGER.info(f"Chargers connected: {self.chargers_connected()}")
+            _LOGGER.debug(f"Charger {cp_id} connected.")
             await cp.start()
         finally:
             self._connected_charger = None
 
-    def chargers_connected(self):
-        """Return number of chargers connected."""
+    def get_metric(self, measurand: str):
+        """Return last known value for given measurand."""
         if self._connected_charger is not None:
-            return 1
-        return 0
+            return self._connected_charger.get_metric(measurand)
+
+        return None
+
+    def get_unit(self, measurand: str):
+        """Return unit of given measurand."""
+        if self._connected_charger is not None:
+            return self._connected_charger.get_unit(measurand)
+
+        return None
 
 
 class ChargePoint(cp):
@@ -92,6 +133,9 @@ class ChargePoint(cp):
         # Indicates if the charger requires a reboot to apply new
         # configuration.
         self._requires_reboot = False
+
+        self._metrics = {}
+        self._units = {}
 
     async def post_connect(self):
         """Logic to be executed right after a charger connects."""
@@ -119,13 +163,13 @@ class ChargePoint(cp):
         for key_value in resp.configuration_key:
             # If the key already has the targeted value we don't need to set
             # it.
-            if key_value.key == key and key_value.value == value:
+            if key_value["key"] == key and key_value["value"] == value:
                 return
 
-            if key_value.read_only:
+            if key_value.get("readonly", False):
                 raise ConfigurationError(f"'{key}' is a read only setting")
 
-        req = await call.ChangeConfigurationPayload(key=key, value=value)
+        req = call.ChangeConfigurationPayload(key=key, value=value)
 
         resp = await self.call(req)
 
@@ -163,36 +207,48 @@ class ChargePoint(cp):
     @on(Action.MeterValues)
     def on_meter_values(self, connector_id: int, meter_value: Dict, **kwargs):
         """Request handler for MeterValues Calls."""
+        for bucket in meter_value:
+            for sampled_value in bucket["sampled_value"]:
+                self._metrics[sampled_value["measurand"]] = sampled_value["value"]
+                self._units[sampled_value["measurand"]] = sampled_value["unit"]
+
         return call_result.MeterValuesPayload()
+
+    def get_metric(self, measurand: str):
+        """Return last known value for given measurand."""
+        return self._metrics.get(measurand, None)
+
+    def get_unit(self, measurand: str):
+        """Return unit of given measurand."""
+        return self._units.get(measurand, None)
 
 
 class ChargeMetric(Entity):
     """Sensor for charge metrics."""
 
-    def __init__(self, data):
+    def __init__(self, measurand, data):
         """Instantiate instance of a ChargeMetrics."""
-        self._name = "Chargers connected"
+        self.measurand = measurand
         self.data = data
         self._state = None
 
-        self._unit_of_measurement = "#"
         self.type = "connected_chargers"
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
+        return self.measurand
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        _LOGGER.info("get state")
-        return self.data.chargers_connected()
+        v = self.data.get_metric(self.measurand)
+        return v
 
     @property
     def unit_of_measurement(self):
         """Return the unit the value is expressed in."""
-        return "Chargers connected"
+        return self.data.get_unit(self.measurand)
 
     @property
     def icon(self):
@@ -201,7 +257,7 @@ class ChargeMetric(Entity):
 
     def update(self):
         """Get the latest data and updates the states."""
-        _LOGGER.info("get update")
+        pass
 
 
 class ConfigurationError(Exception):
